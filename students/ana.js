6b66720ac9b9262ae6a7fa99e3950d60db5ca54a
Ana Moreno <anamariamoreno@gmail.com>
2018-09-24 13:03:40 +0200

    update ana.js

--- students/ana.js
+++ students/ana.js
@@ -6,6 +6,7 @@ function main(){
 var button= document.createElement('button');
 button.innerText = 'Ocultar'
 
+
 var header = document.querySelector('header');
 header.appendChild(button);
 
@@ -14,7 +15,11 @@ var h1 = document.querySelector ('h1');
 
 button.addEventListener('click', function(e){
   h1.classList.toggle('hidden');
-  e.currentTarget.innerText = 'Mostrar';
+  //e.currentTarget.innerText = 'Mostrar';
+  if (h1.classList.contains('hidden')){
+    e.currentTarget.innerText = "Mostrar";
+
+  }else {e.currentTarget.innerText = "Ocultar"}
 
 })
 }
