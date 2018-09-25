f07b3484e2b09ddf6f96ed95f76c9477d60457fe
Ana Moreno <anamariamoreno@gmail.com>
2018-09-25 09:32:26 +0200

    update querySelector

--- students/ana.js
+++ students/ana.js
@@ -6,17 +6,16 @@ function main(){
 var button= document.createElement('button');
 button.innerText = 'Ocultar'
 
+var section = document.querySelector('.profile');
+var p = document.querySelector('p.profile-info');
+section.insertBefore(button,p);
 
-var header = document.querySelector('header');
-header.appendChild(button);
-
-var h1 = document.querySelector ('h1');
 
 
 button.addEventListener('click', function(e){
-  h1.classList.toggle('hidden');
+  p.classList.toggle('hidden');
   //e.currentTarget.innerText = 'Mostrar';
-  if (h1.classList.contains('hidden')){
+  if (p.classList.contains('hidden')){
     e.currentTarget.innerText = "Mostrar";
 
   }else {e.currentTarget.innerText = "Ocultar"}
